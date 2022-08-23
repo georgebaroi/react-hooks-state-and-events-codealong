85f684aeeb75518580870a1995186a90dbb19325
George Baroi <baroi.george@gmail.com>
2022-08-23 02:10:40 -0400

    Done with assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,9 @@ import React from "react";
 import Toggle from "./Toggle";
 
 function App() {
+  
+  
+  
   return (
     <div>
       <h3>Toggle</h3>
--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
 
-function Toggle() {
-  return <button>OFF</button>;
+function Toggle(){
+  
+  //when using useState, the first thing under component function should be the state and setter
+const [isOn, setIsOn] = useState(false)
+
+  //since we're using useState, it is always implied that we will need a handler function to change states for us
+  //inside of the handler function we use the component state and the setter function to tell react what to do
+function handleClick(){
+  setIsOn((isOn)=> !isOn)
+}
+
+//we create const color which is subject to change based on the response from the ternary 
+//
+const color = isOn ? 'red' : 'green'
+
+ return(
+    <button onClick={handleClick} style={{background:color}}>{isOn ? 'On' : 'Off'}</button>
+  )
 }
 
 export default Toggle;
